27758734d369c7eb0d26e6e5f3f1d557146d9505
Naoki Kimura <n.kimura.cap@gmail.com>
2020-05-15 22:34:45 +0900

    Fix a bug

--- src/analyzer.ts
+++ src/analyzer.ts
@@ -32,7 +32,7 @@ export default class Analyzer extends StaticCodeAnalyzer {
   protected async prepare(): Promise<void> {
     console.log(`::group::Installing gems...`);
     try {
-      new installer.RubyGemsInstaller(true).execute(Analyzer.command);
+      await new installer.RubyGemsInstaller(true).execute([Analyzer.command]);
     } finally {
       console.log(`::endgroup::`)
     }
